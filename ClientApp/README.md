37c0e2a9b343eccfb97f191aed9dabd60704f922
Aditya Samantaray <aditya.samantaray1@gmail.com>
2020-09-25 21:35:18 +0530

    Add build file

--- ClientApp/README.md
+++
@@ -1,6 +0,0 @@
-
-## GITHUB COMPARATOR
-
-This is a single page application which lets people to compare Github profiles on the basis of number of followers, number of following people and the number of public repositories. 
-
-The user can enter a valid username and it gets added to the dashboard. The user can delete some user if not required for comparison.
---
+++ ClientApp/build/precache-manifest.46d8ad31a23fbce5240bc1a51594bae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eabe5a4dc03502a1fb23cb9b43b84f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53c100150fcbc6c798a3",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "530e087ac3b990fbea40",
+    "url": "/static/css/main.e3b0cf79.chunk.css"
+  },
+  {
+    "revision": "53c100150fcbc6c798a3",
+    "url": "/static/js/2.988e84f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.988e84f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530e087ac3b990fbea40",
+    "url": "/static/js/main.b0b4d7f4.chunk.js"
+  },
+  {
+    "revision": "ca6232bae7cf01972d0d",
+    "url": "/static/js/runtime-main.35439f5c.js"
+  }
+]);
